f7d5af42911b080ebfd2eefca08314945110d868
nandoltw <nandoltw@rose-hulman.edu>
2018-09-08 15:57:26 -0400

    Done

--- src/m7_summing.py
+++ src/m7_summing.py
@@ -4,9 +4,9 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
+         their colleagues and Thomas Nandola.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
+import math
 
 def main():
     """ Calls the   TEST   functions in this module. """
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_cosines():
     """ Tests the   sum_cosines   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # DONE: 2. Implement this function.
     #   It TESTS the  sum_cosines  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,6 +28,22 @@ def run_test_sum_cosines():
     print('--------------------------------------------------')
     print('Testing the   sum_cosines   function:')
     print('--------------------------------------------------')
+
+    expected = 0.13416
+    answer = sum_cosines(3)
+    print('Example 1 expected', expected)
+    print('          actual', answer)
+
+    expected = -0.51948
+    answer = sum_cosines(4)
+    print('Example 2 expected', expected)
+    print('          actual', answer)
+
+    expected = -0.23581
+    answer = sum_cosines(5)
+    print('Example 3 expected', expected)
+    print('          actual', answer)
+
 
 
 def sum_cosines(n):
@@ -41,19 +57,23 @@ def sum_cosines(n):
         cos(0) + cos(1) + cos(2) + cos(3)   which is about 0.13416.
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_cosines  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(n+1):
+        total = total + math.cos(k)
+    return(total)
 
 
 def run_test_sum_square_roots():
     """ Tests the   sum_square_roots   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # DONE: 4. Implement this function.
     #   It TESTS the  sum_square_roots  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -64,6 +84,22 @@ def run_test_sum_square_roots():
     print('--------------------------------------------------')
     print('Testing the   sum_square_roots   function:')
     print('--------------------------------------------------')
+
+    expected = 11.854408
+    answer = sum_square_roots(5)
+    print('Example 1 expected', expected)
+    print('          actual', answer)
+
+    expected = 19.060167
+    answer = sum_square_roots(7)
+    print('Example 2 expected', expected)
+    print('          actual', answer)
+
+    expected = 31.774943
+    answer = sum_square_roots(10)
+    print('Example 3 expected', expected)
+    print('          actual', answer)
+
 
 
 def sum_square_roots(n):
@@ -80,13 +116,17 @@ def sum_square_roots(n):
       which is about 11.854408.
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #   That is called TEST-DRIVEN DEVELOPMENT (TDD).
     #
     #   No fair running the code of  sum_square_roots  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(n):
+        total = total + math.sqrt((k+1)*(2))
+    return(total)
 
 
 # ----------------------------------------------------------------------
